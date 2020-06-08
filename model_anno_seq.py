6e3ae4a7d57f369557e4cedcc8dfd42f78981a65
jburke11 <56128467+jburke11@users.noreply.github.com>
2020-06-08 09:01:35 -0400

    Update model_anno_seq.py

--- model_anno_seq.py
+++ model_anno_seq.py
@@ -9,20 +9,20 @@ base.metadata.create_all ( engine )
 Session = sessionmaker ( engine )
 session = Session ( )
 
-#query = session.query(Model_anno.transcript_id)
-#for item in query:
-#    new_col = Model_anno_seq(transcript_id=item.transcript_id)
-#    session.add(new_col)
-#session.commit()
-#print("ids in")
-#for record in SeqIO.parse("/Users/burkej24/Downloads/DM_1-3_516_R44_potato.v6.1.hc_gene_models.cds.fa", "fasta"):
-#    query = session.query(Model_anno_seq).filter(Model_anno_seq.transcript_id == record.id)
-#    query = query.first()
-#    try:
-#        query.cds = str(record.seq)
-#    except:
-#        print("id not found")
-#print("cds done")
+query = session.query(Model_anno.transcript_id)
+for item in query:
+    new_col = Model_anno_seq(transcript_id=item.transcript_id)
+    session.add(new_col)
+session.commit()
+print("ids in")
+for record in SeqIO.parse("/Users/burkej24/Downloads/DM_1-3_516_R44_potato.v6.1.hc_gene_models.cds.fa", "fasta"):
+    query = session.query(Model_anno_seq).filter(Model_anno_seq.transcript_id == record.id)
+    query = query.first()
+    try:
+        query.cds = str(record.seq)
+    except:
+        print("id not found")
+print("cds done")
 for record in SeqIO.parse("/Users/burkej24/Downloads/DM_1-3_516_R44_potato.v6.1.hc_gene_models.cdna.fa", "fasta"):
     query = session.query(Model_anno_seq).filter(Model_anno_seq.transcript_id == record.id)
     query = query.first()
